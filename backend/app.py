389fd5cea96f17df6883482878cfe805c2b367cb
Ashan Jayakody <jayakodyashan25@gmail.com>
2025-09-25 15:46:13 +0530

    cores fix

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ scheduler = BackgroundScheduler()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # React dev server
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
