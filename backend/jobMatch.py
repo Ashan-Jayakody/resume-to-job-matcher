bf828b679a8dfe0a3496380815928e1f51537bdf
Ashan Jayakody <jayakodyashan25@gmail.com>
2025-11-20 23:32:14 +0530

    hardcoded jobs

--- backend/jobMatch.py
+++ backend/jobMatch.py
@@ -5,7 +5,7 @@ from getDbConnection import get_db_connection
 # Skills list to check
 skill_keywords = [
     "python", "java", "sql", "react", "javascript", "css",
-    "django", "html", "c++", "node", "express", "mongodb"
+    "django", "html", "c++", "node", "express", "mongodb", "designer", "sales"
 ]
 
 def extract_skills(text: str):
@@ -23,15 +23,16 @@ def match_jobs(user_skills: list):
         cur.execute("SELECT title, link, description, company, location FROM jobs")
         jobs_list = cur.fetchall()
        
-       
-       # ---------------- DEBUGGING ----------------
+        # ---------------- DEBUGGING ----------------
         print("\n========== DEBUG: JOBS FETCHED ==========")
         print(f"Total jobs fetched: {len(jobs_list)}")
         for i, job in enumerate(jobs_list, start=1):
             print(f"Job {i}: {job}")
         print("=========================================\n")
         # -----------------------------------------
-        
+
+
+       
     except psycopg2.Error as e:
         print(f"Error fetch from table: {e}")
     finally:
