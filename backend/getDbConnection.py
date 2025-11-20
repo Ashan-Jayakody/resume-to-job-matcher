81b0c5baf805afaa703dc2fdc98c94d09e6843a6
Ashan Jayakody <jayakodyashan25@gmail.com>
2025-11-20 21:32:56 +0530

    db host change

--- backend/getDbConnection.py
+++ backend/getDbConnection.py
@@ -5,11 +5,10 @@ import psycopg2
 load_dotenv() # load variables from .env
 
 def get_db_connection():
-    return psycopg2.connect(
-        host = os.environ['DB_HOST'],
-        database = os.environ['DB_NAME'],
-        user = os.environ['DB_USER'],
-        password = os.environ['DB_PASSWORD'],
-        port = os.environ.get('DB_PORT', 5432),
-        sslmode = 'require' 
-    )
+
+    postgres_url = os.environ.get('POSTGRES_URL')
+    
+    if postgres_url:
+        print("Connecting using POSTGRES_URL")
+        return psycopg2.connect(postgres_url)
+    
